b27d43dd2f1bc0b73cdccb0d5739c59838f20a6f
Mcalmic <69126774+Mcalmic@users.noreply.github.com>
2021-09-16 19:49:08 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAUzP_GS67oM74Jilp3XEBP2OeyAW8Dppk",
+    authDomain: "storytelling-a-ced7d.firebaseapp.com",
+    projectId: "storytelling-a-ced7d",
+    storageBucket: "storytelling-a-ced7d.appspot.com",
+    messagingSenderId: "994120350969",
+    appId: "1:994120350969:web:a0355361289b883e89b628"
+};
